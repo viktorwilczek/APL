6934f06eaabadd59ad5f210e62cadc636fb76958
vikwil <viktwil638@student.polsl.pl>
2021-11-16 09:32:54 +0100

    Add project files.

---
+++ CS-IMPL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS_IMPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
